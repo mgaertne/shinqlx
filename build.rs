4e212911879e8c1928376aca8197ab24f1bf2306
mgaertne <mgaertne@gmail.com>
2023-05-30 19:46:16 +0200

    let's see if this fixes the github build

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 fn main() {
     let python_config = pyo3_build_config::get();
 
+    if let Some(lib_dir) = &python_config.lib_dir {
+        println!("cargo:rustc-link-search={}", lib_dir);
+    }
+
     if let Some(library) = &python_config.lib_name {
         println!("cargo:rustc-link-lib={}", library);
     }
