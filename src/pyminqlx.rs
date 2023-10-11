a61094fc2fc6af8e512bf19e18020ab5e0aa0457
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-11 23:45:25 +0200

    extended get_userinfo tests

--- src/pyminqlx.rs
+++ src/pyminqlx.rs
@@ -2316,11 +2316,14 @@ fn get_userinfo(py: Python<'_>, client_id: i32) -> PyResult<Option<String>> {
 #[cfg(test)]
 #[cfg(not(miri))]
 mod get_userinfo_tests {
-    use super::get_userinfo;
     use super::MAIN_ENGINE;
+    use super::{get_userinfo, ALLOW_FREE_CLIENT};
+    use crate::client::MockClient;
     use crate::prelude::*;
     use crate::pyminqlx::pyminqlx_setup_fixture::*;
-    use pyo3::exceptions::PyEnvironmentError;
+    use crate::quake_live_engine::MockQuakeEngine;
+    use core::sync::atomic::Ordering;
+    use pyo3::exceptions::{PyEnvironmentError, PyValueError};
     use pyo3::prelude::*;
     use rstest::rstest;
 
@@ -2332,6 +2335,101 @@ mod get_userinfo_tests {
             let result = get_userinfo(py, 0);
             assert!(result.is_err_and(|err| err.is_instance_of::<PyEnvironmentError>(py)));
         })
+    }
+
+    #[rstest]
+    #[serial]
+    fn get_userinfo_for_client_id_below_zero(_pyminqlx_setup: ()) {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+        Python::with_gil(|py| {
+            let result = get_userinfo(py, -1);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        })
+    }
+
+    #[rstest]
+    #[serial]
+    fn get_userinfo_for_client_id_above_max_clients(_pyminqlx_setup: ()) {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+        Python::with_gil(|py| {
+            let result = get_userinfo(py, 42);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        })
+    }
+
+    #[rstest]
+    #[serial]
+    fn get_userinfo_for_existing_client(_pyminqlx_setup: ()) {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx.expect().returning(|_client_id| {
+            let mut mock_client = MockClient::new();
+            mock_client
+                .expect_get_state()
+                .returning(|| clientState_t::CS_ACTIVE);
+            mock_client
+                .expect_get_user_info()
+                .returning(|| "asdf".into());
+            mock_client
+        });
+
+        let userinfo = Python::with_gil(|py| get_userinfo(py, 2).unwrap());
+        assert_eq!(userinfo, Some("asdf".into()));
+    }
+
+    #[rstest]
+    #[serial]
+    fn get_userinfo_for_non_allowed_free_client(_pyminqlx_setup: ()) {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+        ALLOW_FREE_CLIENT.store(0, Ordering::SeqCst);
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx.expect().returning(|_client_id| {
+            let mut mock_client = MockClient::new();
+            mock_client
+                .expect_get_state()
+                .returning(|| clientState_t::CS_FREE);
+            mock_client
+                .expect_get_user_info()
+                .returning(|| "asdf".into());
+            mock_client
+        });
+
+        let userinfo = Python::with_gil(|py| get_userinfo(py, 2).unwrap());
+        assert_eq!(userinfo, None);
+    }
+
+    #[rstest]
+    #[serial]
+    fn get_userinfo_for_allowed_free_client(_pyminqlx_setup: ()) {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+        ALLOW_FREE_CLIENT.store(2, Ordering::SeqCst);
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx.expect().returning(|_client_id| {
+            let mut mock_client = MockClient::new();
+            mock_client
+                .expect_get_state()
+                .returning(|| clientState_t::CS_FREE);
+            mock_client
+                .expect_get_user_info()
+                .returning(|| "asdf".into());
+            mock_client
+        });
+
+        let userinfo = Python::with_gil(|py| get_userinfo(py, 2).unwrap());
+        assert_eq!(userinfo, Some("asdf".into()));
     }
 }
 
