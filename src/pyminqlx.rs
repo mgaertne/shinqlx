4d7e7e4e03d526bb0753cf7e438025c70b2941e5
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-15 14:21:50 +0200

    extended kick tests

--- src/pyminqlx.rs
+++ src/pyminqlx.rs
@@ -3051,19 +3051,19 @@ fn kick(py: Python<'_>, client_id: i32, reason: Option<&str>) -> PyResult<()> {
 
     if !(0..maxclients).contains(&client_id) {
         return Err(PyValueError::new_err(format!(
-            "client_id needs to be a number from 0 to {}, or None.",
+            "client_id needs to be a number from 0 to {}.",
             maxclients - 1
         )));
     }
 
     py.allow_threads(move || match Client::try_from(client_id) {
         Err(_) => Err(PyValueError::new_err(
-            "client_id must be None or the ID of an active player.",
+            "client_id must be the ID of an active player.",
         )),
         Ok(mut client) => {
             if client.get_state() != clientState_t::CS_ACTIVE {
                 return Err(PyValueError::new_err(
-                    "client_id must be None or the ID of an active player.",
+                    "client_id must be the ID of an active player.",
                 ));
             }
             let reason_str = if reason.unwrap_or("was kicked.").is_empty() {
@@ -3083,9 +3083,14 @@ fn kick(py: Python<'_>, client_id: i32, reason: Option<&str>) -> PyResult<()> {
 mod kick_tests {
     use super::kick;
     use super::MAIN_ENGINE;
+    use crate::client::MockClient;
+    use crate::hooks::mock_hooks::shinqlx_drop_client_context;
     use crate::prelude::*;
-    use pyo3::exceptions::PyEnvironmentError;
+    use crate::quake_live_engine::MockQuakeEngine;
+    use mockall::predicate;
+    use pyo3::exceptions::{PyEnvironmentError, PyValueError};
     use pyo3::prelude::*;
+    use rstest::rstest;
 
     #[test]
     #[serial]
@@ -3094,6 +3099,121 @@ mod kick_tests {
         Python::with_gil(|py| {
             let result = kick(py, 0, None);
             assert!(result.is_err_and(|err| err.is_instance_of::<PyEnvironmentError>(py)));
+        })
+    }
+
+    #[test]
+    #[serial]
+    fn kick_with_client_id_below_zero() {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        Python::with_gil(|py| {
+            let result = kick(py, -1, None);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        })
+    }
+
+    #[test]
+    #[serial]
+    fn kick_with_client_id_too_large() {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        Python::with_gil(|py| {
+            let result = kick(py, 42, None);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        })
+    }
+
+    #[rstest]
+    #[case(clientState_t::CS_FREE)]
+    #[case(clientState_t::CS_CONNECTED)]
+    #[case(clientState_t::CS_PRIMED)]
+    #[case(clientState_t::CS_ZOMBIE)]
+    #[serial]
+    fn kick_with_client_id_for_non_active_client(#[case] clientstate: clientState_t) {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(move |_| {
+                let mut mock_client = MockClient::new();
+                mock_client.expect_get_state().return_const(clientstate);
+                mock_client
+            });
+
+        Python::with_gil(|py| {
+            let result = kick(py, 2, None);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        })
+    }
+
+    #[test]
+    #[serial]
+    fn kick_with_client_id_for_active_client_without_kick_reason() {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(move |_| {
+                let mut mock_client = MockClient::new();
+                mock_client
+                    .expect_get_state()
+                    .return_const(clientState_t::CS_ACTIVE);
+                mock_client
+            });
+
+        let drop_client_ctx = shinqlx_drop_client_context();
+        drop_client_ctx
+            .expect()
+            .withf(|_, reason| reason == "was kicked.")
+            .times(1);
+
+        Python::with_gil(|py| {
+            let result = kick(py, 2, None);
+            assert!(result.is_ok());
+        })
+    }
+
+    #[test]
+    #[serial]
+    fn kick_with_client_id_for_active_client_with_kick_reason() {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(move |_| {
+                let mut mock_client = MockClient::new();
+                mock_client
+                    .expect_get_state()
+                    .return_const(clientState_t::CS_ACTIVE);
+                mock_client
+            });
+
+        let drop_client_ctx = shinqlx_drop_client_context();
+        drop_client_ctx
+            .expect()
+            .withf(|_, reason| reason == "please go away!")
+            .times(1);
+
+        Python::with_gil(|py| {
+            let result = kick(py, 2, Some("please go away!"));
+            assert!(result.is_ok());
         })
     }
 }
