e96b6e3febcd52e57ef011ebe4d7f05b8266d532
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-17 01:24:55 +0200

    extended player_state tests

--- src/pyminqlx.rs
+++ src/pyminqlx.rs
@@ -3463,6 +3463,18 @@ mod force_vote_tests {
         });
     }
 
+    #[serial]
+    fn force_vote_when_no_vote_is_running() {
+        let current_level_try_get_ctx = MockTestCurrentLevel::try_get_context();
+        current_level_try_get_ctx
+            .expect()
+            .returning(|| Err(QuakeLiveEngineError::MainEngineNotInitialized));
+        MAIN_ENGINE.store(None);
+
+        let result = Python::with_gil(|py| force_vote(py, false)).unwrap();
+        assert_eq!(result, false);
+    }
+
     #[rstest]
     #[case(clientState_t::CS_ZOMBIE)]
     #[case(clientState_t::CS_CONNECTED)]
@@ -4557,8 +4569,9 @@ fn player_state(py: Python<'_>, client_id: i32) -> PyResult<Option<PlayerState>>
 #[cfg(test)]
 #[cfg(not(miri))]
 mod player_state_tests {
-    use super::player_state;
-    use super::MAIN_ENGINE;
+    use super::{player_state, Holdable, PlayerState, Vector3};
+    use super::{Flight, Powerups, Weapons, MAIN_ENGINE};
+    use crate::game_client::MockGameClient;
     use crate::game_entity::MockGameEntity;
     use crate::prelude::*;
     use crate::quake_live_engine::MockQuakeEngine;
@@ -4624,6 +4637,83 @@ mod player_state_tests {
 
         let result = Python::with_gil(|py| player_state(py, 2)).unwrap();
         assert_eq!(result, None);
+    }
+
+    #[test]
+    #[serial]
+    fn player_state_transforms_from_game_client() {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_get_game_client().returning(|| {
+                    let mut mock_game_client = MockGameClient::new();
+                    mock_game_client
+                        .expect_get_position()
+                        .returning(|| (1.0, 2.0, 3.0));
+                    mock_game_client
+                        .expect_get_velocity()
+                        .returning(|| (4.0, 5.0, 6.0));
+                    mock_game_client.expect_is_alive().returning(|| true);
+                    mock_game_client.expect_get_armor().returning(|| 456);
+                    mock_game_client.expect_get_noclip().returning(|| true);
+                    mock_game_client
+                        .expect_get_weapon()
+                        .returning(|| weapon_t::WP_NAILGUN);
+                    mock_game_client
+                        .expect_get_weapons()
+                        .returning(|| [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1]);
+                    mock_game_client
+                        .expect_get_ammos()
+                        .returning(|| [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+                    mock_game_client
+                        .expect_get_powerups()
+                        .returning(|| [12, 34, 56, 78, 90, 24]);
+                    mock_game_client
+                        .expect_get_holdable()
+                        .returning(|| Holdable::Kamikaze.into());
+                    mock_game_client
+                        .expect_get_current_flight_fuel()
+                        .returning(|| 12);
+                    mock_game_client
+                        .expect_get_max_flight_fuel()
+                        .returning(|| 34);
+                    mock_game_client.expect_get_flight_thrust().returning(|| 56);
+                    mock_game_client.expect_get_flight_refuel().returning(|| 78);
+                    mock_game_client.expect_is_chatting().returning(|| true);
+                    mock_game_client.expect_is_frozen().returning(|| true);
+                    Ok(mock_game_client)
+                });
+                mock_game_entity.expect_get_health().returning(|| 123);
+                mock_game_entity
+            });
+
+        let result = Python::with_gil(|py| player_state(py, 2)).unwrap();
+        assert_eq!(
+            result,
+            Some(PlayerState {
+                is_alive: true,
+                position: Vector3(1, 2, 3),
+                velocity: Vector3(4, 5, 6),
+                health: 123,
+                armor: 456,
+                noclip: true,
+                weapon: weapon_t::WP_NAILGUN.into(),
+                weapons: Weapons(1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1),
+                ammo: Weapons(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+                powerups: Powerups(12, 34, 56, 78, 90, 24),
+                holdable: Some("kamikaze".into()),
+                flight: Flight(12, 34, 56, 78),
+                is_chatting: true,
+                is_frozen: true,
+            })
+        );
     }
 }
 
