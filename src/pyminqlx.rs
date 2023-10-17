1a0115b031ef8f2acf42d131d758d135619edd29
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-17 22:10:48 +0200

    extended player_stats tests

--- src/pyminqlx.rs
+++ src/pyminqlx.rs
@@ -4856,8 +4856,10 @@ fn player_stats(py: Python<'_>, client_id: i32) -> PyResult<Option<PlayerStats>>
 #[cfg(test)]
 #[cfg(not(miri))]
 mod player_stats_tests {
-    use super::player_stats;
     use super::MAIN_ENGINE;
+    use super::{player_stats, PlayerStats};
+    use crate::game_client::MockGameClient;
+    use crate::game_entity::MockGameEntity;
     use crate::prelude::*;
     use crate::quake_live_engine::MockQuakeEngine;
     use pyo3::exceptions::{PyEnvironmentError, PyValueError};
@@ -4897,6 +4899,47 @@ mod player_stats_tests {
             let result = player_stats(py, 666);
             assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
         });
+    }
+
+    #[test]
+    #[serial]
+    fn player_stats_for_game_client() {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 16);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_get_game_client().returning(|| {
+                let mut mock_game_client = MockGameClient::new();
+                mock_game_client.expect_get_score().returning(|| 42);
+                mock_game_client.expect_get_kills().returning(|| 7);
+                mock_game_client.expect_get_deaths().returning(|| 9);
+                mock_game_client
+                    .expect_get_damage_dealt()
+                    .returning(|| 5000);
+                mock_game_client
+                    .expect_get_damage_taken()
+                    .returning(|| 4200);
+                mock_game_client.expect_get_time_on_team().returning(|| 123);
+                mock_game_client.expect_get_ping().returning(|| 9);
+                Ok(mock_game_client)
+            });
+            mock_game_entity
+        });
+        let result = Python::with_gil(|py| player_stats(py, 2)).unwrap();
+
+        assert!(result.is_some_and(|pstats| pstats
+            == PlayerStats {
+                score: 42,
+                kills: 7,
+                deaths: 9,
+                damage_dealt: 5000,
+                damage_taken: 4200,
+                time: 123,
+                ping: 9,
+            }));
     }
 }
 
