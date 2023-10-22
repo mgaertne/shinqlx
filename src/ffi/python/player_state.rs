dd7e27b92ec239bb1ddc5228bca5616735c1ef85
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-22 19:23:24 +0200

    extended PlayerState tests

--- src/ffi/python/player_state.rs
+++ src/ffi/python/player_state.rs
@@ -114,3 +114,153 @@ impl From<GameEntity> for PlayerState {
         }
     }
 }
+
+#[cfg(test)]
+mod player_state_tests {
+    use super::PlayerState;
+    use crate::ffi::c::game_client::MockGameClient;
+    use crate::ffi::c::game_entity::MockGameEntity;
+    use crate::ffi::python::{Flight, Holdable, Powerups, Vector3, Weapons};
+    use crate::prelude::weapon_t;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn player_state_from_game_client() {
+        let mut mock_game_entity = MockGameEntity::new();
+        mock_game_entity.expect_get_game_client().returning(|| {
+            let mut mock_game_client = MockGameClient::new();
+            mock_game_client
+                .expect_get_position()
+                .returning(|| (1.0, 2.0, 3.0));
+            mock_game_client
+                .expect_get_velocity()
+                .returning(|| (4.0, 5.0, 6.0));
+            mock_game_client.expect_is_alive().returning(|| true);
+            mock_game_client.expect_get_armor().returning(|| 456);
+            mock_game_client.expect_get_noclip().returning(|| true);
+            mock_game_client
+                .expect_get_weapon()
+                .returning(|| weapon_t::WP_NAILGUN);
+            mock_game_client
+                .expect_get_weapons()
+                .returning(|| [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1]);
+            mock_game_client
+                .expect_get_ammos()
+                .returning(|| [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+            mock_game_client
+                .expect_get_powerups()
+                .returning(|| [12, 34, 56, 78, 90, 24]);
+            mock_game_client
+                .expect_get_holdable()
+                .returning(|| Holdable::Kamikaze.into());
+            mock_game_client
+                .expect_get_current_flight_fuel()
+                .returning(|| 12);
+            mock_game_client
+                .expect_get_max_flight_fuel()
+                .returning(|| 34);
+            mock_game_client.expect_get_flight_thrust().returning(|| 56);
+            mock_game_client.expect_get_flight_refuel().returning(|| 78);
+            mock_game_client.expect_is_chatting().returning(|| true);
+            mock_game_client.expect_is_frozen().returning(|| true);
+            Ok(mock_game_client)
+        });
+        mock_game_entity.expect_get_health().returning(|| 123);
+
+        assert_eq!(
+            PlayerState::from(mock_game_entity),
+            PlayerState {
+                is_alive: true,
+                position: Vector3(1, 2, 3),
+                velocity: Vector3(4, 5, 6),
+                health: 123,
+                armor: 456,
+                noclip: true,
+                weapon: weapon_t::WP_NAILGUN.into(),
+                weapons: Weapons(1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1),
+                ammo: Weapons(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+                powerups: Powerups(12, 34, 56, 78, 90, 24),
+                holdable: Some("kamikaze".into()),
+                flight: Flight(12, 34, 56, 78),
+                is_chatting: true,
+                is_frozen: true,
+            }
+        );
+    }
+
+    #[test]
+    fn player_state_to_str() {
+        let player_state = PlayerState {
+            is_alive: true,
+            position: Vector3(1, 2, 3),
+            velocity: Vector3(4, 5, 6),
+            health: 123,
+            armor: 456,
+            noclip: true,
+            weapon: weapon_t::WP_NAILGUN.into(),
+            weapons: Weapons(1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1),
+            ammo: Weapons(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+            powerups: Powerups(12, 34, 56, 78, 90, 24),
+            holdable: Some("kamikaze".into()),
+            flight: Flight(12, 34, 56, 78),
+            is_chatting: true,
+            is_frozen: true,
+        };
+
+        assert_eq!(
+            player_state.__str__(),
+            "PlayerState(\
+            is_alive=true, \
+            position=Vector3(x=1, y=2, z=3), \
+            veclocity=Vector3(x=4, y=5, z=6), \
+            health=123, \
+            armor=456, \
+            noclip=true, \
+            weapon=11, \
+            weapons=Weapons(g=1, mg=1, sg=1, gl=0, rl=0, lg=0, rg=0, pg=1, bfg=1, gh=0, ng=0, pl=0, cg=1, hmg=1, hands=1), \
+            ammo=Weapons(g=1, mg=2, sg=3, gl=4, rl=5, lg=6, rg=6, pg=8, bfg=9, gh=10, ng=11, pl=12, cg=13, hmg=14, hands=15), \
+            powerups=Powerups(quad=12, battlesuit=34, haste=56, invisibility=78, regeneration=90, invulnerability=24), \
+            holdable=kamikaze, \
+            flight=Flight(fuel=12, max_fuel=34, thrust=56, refuel=78), \
+            is_chatting=true, \
+            is_frozen=true)");
+    }
+
+    #[test]
+    fn player_state_repr() {
+        let player_state = PlayerState {
+            is_alive: true,
+            position: Vector3(1, 2, 3),
+            velocity: Vector3(4, 5, 6),
+            health: 123,
+            armor: 456,
+            noclip: true,
+            weapon: weapon_t::WP_NAILGUN.into(),
+            weapons: Weapons(1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1),
+            ammo: Weapons(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+            powerups: Powerups(12, 34, 56, 78, 90, 24),
+            holdable: Some("kamikaze".into()),
+            flight: Flight(12, 34, 56, 78),
+            is_chatting: true,
+            is_frozen: true,
+        };
+
+        assert_eq!(
+            player_state.__repr__(),
+            "PlayerState(\
+            is_alive=true, \
+            position=Vector3(x=1, y=2, z=3), \
+            veclocity=Vector3(x=4, y=5, z=6), \
+            health=123, \
+            armor=456, \
+            noclip=true, \
+            weapon=11, \
+            weapons=Weapons(g=1, mg=1, sg=1, gl=0, rl=0, lg=0, rg=0, pg=1, bfg=1, gh=0, ng=0, pl=0, cg=1, hmg=1, hands=1), \
+            ammo=Weapons(g=1, mg=2, sg=3, gl=4, rl=5, lg=6, rg=6, pg=8, bfg=9, gh=10, ng=11, pl=12, cg=13, hmg=14, hands=15), \
+            powerups=Powerups(quad=12, battlesuit=34, haste=56, invisibility=78, regeneration=90, invulnerability=24), \
+            holdable=kamikaze, \
+            flight=Flight(fuel=12, max_fuel=34, thrust=56, refuel=78), \
+            is_chatting=true, \
+            is_frozen=true)");
+    }
+}
