eb510f91972637a1485480bfe20ac036c62dfd69
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-24 00:56:33 +0200

    extended players_info tests

--- src/ffi/python/embed/players_info.rs
+++ src/ffi/python/embed/players_info.rs
@@ -41,7 +41,12 @@ pub(crate) fn minqlx_players_info(py: Python<'_>) -> PyResult<Vec<Option<PlayerI
 mod get_players_info_tests {
     use super::minqlx_players_info;
     use super::MAIN_ENGINE;
+    use crate::ffi::c::client::MockClient;
+    use crate::ffi::c::game_entity::MockGameEntity;
+    use crate::ffi::python::PlayerInfo;
     use crate::prelude::*;
+    use crate::quake_live_engine::MockQuakeEngine;
+    use mockall::predicate;
     use pyo3::exceptions::PyEnvironmentError;
     use pyo3::prelude::*;
 
@@ -53,5 +58,100 @@ mod get_players_info_tests {
             let result = minqlx_players_info(py);
             assert!(result.is_err_and(|err| err.is_instance_of::<PyEnvironmentError>(py)));
         });
+    }
+
+    #[test]
+    #[serial]
+    fn get_players_info_for_existing_clients() {
+        let mut mock_engine = MockQuakeEngine::new();
+        mock_engine.expect_get_max_clients().returning(|| 3);
+        MAIN_ENGINE.store(Some(mock_engine.into()));
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx
+            .expect()
+            .with(predicate::eq(0))
+            .returning(|_client_id| {
+                let mut mock_client = MockClient::new();
+                mock_client
+                    .expect_get_state()
+                    .returning(|| clientState_t::CS_ACTIVE);
+                mock_client
+                    .expect_get_user_info()
+                    .returning(|| "asdf".into());
+                mock_client.expect_get_steam_id().returning(|| 1234);
+                mock_client
+            });
+
+        client_try_from_ctx
+            .expect()
+            .with(predicate::eq(1))
+            .returning(|_client_id| {
+                let mut mock_client = MockClient::new();
+                mock_client
+                    .expect_get_state()
+                    .returning(|| clientState_t::CS_FREE);
+                mock_client
+                    .expect_get_user_info()
+                    .returning(|| "asdf".into());
+                mock_client.expect_get_steam_id().returning(|| 1234);
+                mock_client
+            });
+
+        let client_try_from_ctx = MockClient::from_context();
+        client_try_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_client_id| {
+                let mut mock_client = MockClient::new();
+                mock_client
+                    .expect_get_state()
+                    .returning(|| clientState_t::CS_ACTIVE);
+                mock_client
+                    .expect_get_user_info()
+                    .returning(|| "asdf".into());
+                mock_client.expect_get_steam_id().returning(|| 1234);
+                mock_client
+            });
+
+        let game_entity_try_from_ctx = MockGameEntity::from_context();
+        game_entity_try_from_ctx.expect().returning(|_client_id| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity
+                .expect_get_player_name()
+                .returning(|| "Mocked Player".into());
+            mock_game_entity
+                .expect_get_team()
+                .returning(|| team_t::TEAM_RED);
+            mock_game_entity
+                .expect_get_privileges()
+                .returning(|| privileges_t::PRIV_NONE);
+            mock_game_entity
+        });
+
+        let players_info = Python::with_gil(|py| minqlx_players_info(py).unwrap());
+        assert_eq!(
+            players_info,
+            vec![
+                Some(PlayerInfo {
+                    client_id: 0,
+                    name: "Mocked Player".into(),
+                    connection_state: clientState_t::CS_ACTIVE as i32,
+                    userinfo: "asdf".into(),
+                    steam_id: 1234,
+                    team: team_t::TEAM_RED as i32,
+                    privileges: privileges_t::PRIV_NONE as i32
+                }),
+                Some(PlayerInfo {
+                    client_id: 2,
+                    name: "Mocked Player".into(),
+                    connection_state: clientState_t::CS_ACTIVE as i32,
+                    userinfo: "asdf".into(),
+                    steam_id: 1234,
+                    team: team_t::TEAM_RED as i32,
+                    privileges: privileges_t::PRIV_NONE as i32
+                })
+            ]
+        );
     }
 }
