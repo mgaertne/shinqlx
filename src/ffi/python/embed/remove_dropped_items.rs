9a2a7bf1513a48ea06f2af3c60fa3c8cac8599b9
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-24 22:22:05 +0200

    added remove_dropped_items tests

--- src/ffi/python/embed/remove_dropped_items.rs
+++ src/ffi/python/embed/remove_dropped_items.rs
@@ -17,3 +17,121 @@ pub(crate) fn minqlx_remove_dropped_items(py: Python<'_>) -> PyResult<bool> {
 
     Ok(true)
 }
+
+#[cfg(test)]
+mod remove_dropped_items_tests {
+    use super::minqlx_remove_dropped_items;
+    use crate::ffi::c::game_entity::MockGameEntity;
+    use crate::prelude::*;
+    use mockall::predicate;
+    use pyo3::prelude::*;
+
+    #[test]
+    #[serial]
+    fn remove_dropped_items_for_unused_entity() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(1))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| false);
+                mock_game_entity.expect_has_flags().returning(|| true);
+                mock_game_entity.expect_is_dropped_item().returning(|| true);
+                mock_game_entity.expect_free_entity().times(0);
+                mock_game_entity
+            });
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity.expect_has_flags().returning(|| false);
+            mock_game_entity
+                .expect_is_dropped_item()
+                .returning(|| false);
+            mock_game_entity.expect_free_entity().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_remove_dropped_items(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn remove_dropped_items_for_entity_without_flags() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(1))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| true);
+                mock_game_entity.expect_has_flags().returning(|| false);
+                mock_game_entity.expect_is_dropped_item().returning(|| true);
+                mock_game_entity.expect_free_entity().times(0);
+                mock_game_entity
+            });
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity.expect_has_flags().returning(|| false);
+            mock_game_entity
+                .expect_is_dropped_item()
+                .returning(|| false);
+            mock_game_entity.expect_free_entity().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_remove_dropped_items(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn remove_dropped_items_for_non_dropped_entity() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(1))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| true);
+                mock_game_entity.expect_has_flags().returning(|| true);
+                mock_game_entity
+                    .expect_is_dropped_item()
+                    .returning(|| false);
+                mock_game_entity.expect_free_entity().times(0);
+                mock_game_entity
+            });
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity.expect_has_flags().returning(|| false);
+            mock_game_entity
+                .expect_is_dropped_item()
+                .returning(|| false);
+            mock_game_entity.expect_free_entity().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_remove_dropped_items(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn remove_dropped_items_for_removable_dropped_entities() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| true);
+            mock_game_entity.expect_has_flags().returning(|| true);
+            mock_game_entity.expect_is_dropped_item().returning(|| true);
+            mock_game_entity.expect_free_entity().times(1);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_remove_dropped_items(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+}
