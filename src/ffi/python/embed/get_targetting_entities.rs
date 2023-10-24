74b03378d87b527fe0dd5dadb9f3f677596ee291
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-24 22:50:39 +0200

    added get_targetting_entities tests

--- src/ffi/python/embed/get_targetting_entities.rs
+++ src/ffi/python/embed/get_targetting_entities.rs
@@ -7,7 +7,7 @@ use pyo3::{pyfunction, PyResult, Python};
 #[pyfunction]
 #[pyo3(name = "get_targetting_entities")]
 pub(crate) fn minqlx_get_entity_targets(py: Python<'_>, entity_id: i32) -> PyResult<Vec<u32>> {
-    if entity_id < 0 || entity_id >= MAX_GENTITIES as i32 {
+    if !(0..MAX_GENTITIES as i32).contains(&entity_id) {
         return Err(PyValueError::new_err(format!(
             "entity_id need to be between 0 and {}.",
             MAX_GENTITIES - 1
@@ -20,4 +20,71 @@ pub(crate) fn minqlx_get_entity_targets(py: Python<'_>, entity_id: i32) -> PyRes
             |entity| Ok(entity.get_targetting_entity_ids()),
         )
     })
+}
+
+#[cfg(test)]
+#[cfg(not(miri))]
+mod get_entity_targets_tests {
+    use super::minqlx_get_entity_targets;
+    use crate::ffi::c::game_entity::MockGameEntity;
+    use crate::prelude::*;
+    use mockall::predicate;
+    use pyo3::exceptions::PyValueError;
+    use pyo3::prelude::*;
+
+    #[test]
+    fn get_entity_targets_for_too_small_entity_id() {
+        Python::with_gil(|py| {
+            let result = minqlx_get_entity_targets(py, -1);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        });
+    }
+
+    #[test]
+    fn get_entity_targets_for_too_large_entity_id() {
+        Python::with_gil(|py| {
+            let result = minqlx_get_entity_targets(py, MAX_GENTITIES as i32);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn get_entity_targets_for_valid_entity_id_with_no_targetting_entities() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity
+                    .expect_get_targetting_entity_ids()
+                    .returning(|| vec![]);
+                mock_game_entity
+            })
+            .times(1);
+
+        let result = Python::with_gil(|py| minqlx_get_entity_targets(py, 2));
+        assert!(result.is_ok_and(|item_ids| item_ids == vec![]));
+    }
+
+    #[test]
+    #[serial]
+    fn get_entity_targets_for_valid_entity_id_with_targetting_entities() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity
+                    .expect_get_targetting_entity_ids()
+                    .returning(|| vec![42, 21, 1337]);
+                mock_game_entity
+            })
+            .times(1);
+
+        let result = Python::with_gil(|py| minqlx_get_entity_targets(py, 2));
+        assert!(result.is_ok_and(|item_ids| item_ids == vec![42, 21, 1337]));
+    }
 }
