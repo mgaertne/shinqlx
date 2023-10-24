ff8352c925d905b42f14289239b59c09f284ca27
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-24 23:13:11 +0200

    added destroy_kamikaze_timers tests

--- src/ffi/python/embed/destroy_kamikaze_timers.rs
+++ src/ffi/python/embed/destroy_kamikaze_timers.rs
@@ -26,3 +26,200 @@ pub(crate) fn minqlx_destroy_kamikaze_timers(py: Python<'_>) -> PyResult<bool> {
         Ok(true)
     })
 }
+
+#[cfg(test)]
+#[cfg(not(miri))]
+mod destroy_kamikaze_timers_tests {
+    use super::minqlx_destroy_kamikaze_timers;
+    use crate::ffi::c::game_client::MockGameClient;
+    use crate::ffi::c::game_entity::MockGameEntity;
+    use crate::prelude::*;
+    use mockall::predicate;
+    use pyo3::prelude::*;
+
+    #[test]
+    #[serial]
+    fn destroy_kamikaze_timers_for_not_in_use_game_entity() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| false);
+                mock_game_entity.expect_get_health().returning(|| 0);
+                mock_game_entity.expect_get_game_client().returning(|| {
+                    let mut mock_game_client = MockGameClient::new();
+                    mock_game_client.expect_remove_kamikaze_flag().times(0);
+                    Ok(mock_game_client)
+                });
+                mock_game_entity
+                    .expect_is_kamikaze_timer()
+                    .returning(|| true);
+                mock_game_entity.expect_free_entity().times(0);
+                mock_game_entity
+            });
+
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity.expect_get_health().returning(|| 42);
+            mock_game_entity
+                .expect_get_game_client()
+                .returning(|| Err(QuakeLiveEngineError::MainEngineNotInitialized));
+            mock_game_entity
+                .expect_is_kamikaze_timer()
+                .returning(|| false);
+            mock_game_entity.expect_free_entity().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_destroy_kamikaze_timers(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn destroy_kamikaze_timers_for_in_use_non_kamikaze_timer() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| true);
+                mock_game_entity.expect_get_health().returning(|| 42);
+                mock_game_entity.expect_get_game_client().returning(|| {
+                    let mut mock_game_client = MockGameClient::new();
+                    mock_game_client.expect_remove_kamikaze_flag().times(0);
+                    Ok(mock_game_client)
+                });
+                mock_game_entity
+                    .expect_is_kamikaze_timer()
+                    .returning(|| false);
+                mock_game_entity.expect_free_entity().times(0);
+                mock_game_entity
+            });
+
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity.expect_get_health().returning(|| 42);
+            mock_game_entity
+                .expect_get_game_client()
+                .returning(|| Err(QuakeLiveEngineError::MainEngineNotInitialized));
+            mock_game_entity
+                .expect_is_kamikaze_timer()
+                .returning(|| false);
+            mock_game_entity.expect_free_entity().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_destroy_kamikaze_timers(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn destroy_kamikaze_timers_for_in_use_kamikaze_timer_with_health() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| true);
+                mock_game_entity.expect_get_health().returning(|| 42);
+                mock_game_entity.expect_get_game_client().returning(|| {
+                    let mut mock_game_client = MockGameClient::new();
+                    mock_game_client.expect_remove_kamikaze_flag().times(0);
+                    Ok(mock_game_client)
+                });
+                mock_game_entity
+                    .expect_is_kamikaze_timer()
+                    .returning(|| true);
+                mock_game_entity.expect_free_entity().times(1);
+                mock_game_entity
+            });
+
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity.expect_get_health().returning(|| 42);
+            mock_game_entity
+                .expect_get_game_client()
+                .returning(|| Err(QuakeLiveEngineError::MainEngineNotInitialized));
+            mock_game_entity
+                .expect_is_kamikaze_timer()
+                .returning(|| false);
+            mock_game_entity.expect_free_entity().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_destroy_kamikaze_timers(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn destroy_kamikaze_timers_for_in_use_kamikaze_timer_with_no_health_but_no_game_client() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| true);
+                mock_game_entity.expect_get_health().returning(|| 0);
+                mock_game_entity
+                    .expect_get_game_client()
+                    .returning(|| Err(QuakeLiveEngineError::MainEngineNotInitialized));
+                mock_game_entity
+                    .expect_is_kamikaze_timer()
+                    .returning(|| true);
+                mock_game_entity.expect_free_entity().times(1);
+                mock_game_entity
+            });
+
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity.expect_get_health().returning(|| 42);
+            mock_game_entity
+                .expect_get_game_client()
+                .returning(|| Err(QuakeLiveEngineError::MainEngineNotInitialized));
+            mock_game_entity
+                .expect_is_kamikaze_timer()
+                .returning(|| false);
+            mock_game_entity.expect_free_entity().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_destroy_kamikaze_timers(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn destroy_kamikaze_timers_for_in_use_kamikaze_timer_with_no_health_but_game_client() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| true);
+            mock_game_entity.expect_get_health().returning(|| 0);
+            mock_game_entity.expect_get_game_client().returning(|| {
+                let mut mock_game_client = MockGameClient::new();
+                mock_game_client.expect_remove_kamikaze_flag().times(1);
+                Ok(mock_game_client)
+            });
+            mock_game_entity
+                .expect_is_kamikaze_timer()
+                .returning(|| true);
+            mock_game_entity.expect_free_entity().times(1);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_destroy_kamikaze_timers(py));
+        assert!(result.is_ok_and(|value| value));
+    }
+}
