ec22e93fdccbf0b5de62a0f71c40f55178802907
Markus Gärtner <markus.gaertner@it-agile.de>
2023-10-24 22:35:54 +0200

    added force_weapon_respawn_time tests

--- src/ffi/python/embed/force_weapon_respawn_time.rs
+++ src/ffi/python/embed/force_weapon_respawn_time.rs
@@ -25,3 +25,103 @@ pub(crate) fn minqlx_force_weapon_respawn_time(
 
     Ok(true)
 }
+
+#[cfg(test)]
+#[cfg(not(miri))]
+mod force_weapon_respawn_time_tests {
+    use super::minqlx_force_weapon_respawn_time;
+    use crate::ffi::c::game_entity::MockGameEntity;
+    use crate::prelude::*;
+    use mockall::predicate;
+    use pyo3::exceptions::PyValueError;
+    use pyo3::prelude::*;
+
+    #[test]
+    fn force_weapon_respawn_time_with_too_small_respawn_time() {
+        Python::with_gil(|py| {
+            let result = minqlx_force_weapon_respawn_time(py, -1);
+            assert!(result.is_err_and(|err| err.is_instance_of::<PyValueError>(py)));
+        })
+    }
+
+    #[test]
+    #[serial]
+    fn force_weapon_respawn_time_with_non_in_use_weapon() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| false);
+                mock_game_entity
+                    .expect_is_respawning_weapon()
+                    .returning(|| true);
+                mock_game_entity.expect_set_respawn_time().times(0);
+                mock_game_entity
+            });
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity
+                .expect_is_respawning_weapon()
+                .returning(|| false);
+            mock_game_entity.expect_set_respawn_time().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_force_weapon_respawn_time(py, 123));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn force_weapon_respawn_time_with_non_respawning_weapon() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx
+            .expect()
+            .with(predicate::eq(2))
+            .returning(|_| {
+                let mut mock_game_entity = MockGameEntity::new();
+                mock_game_entity.expect_in_use().returning(|| true);
+                mock_game_entity
+                    .expect_is_respawning_weapon()
+                    .returning(|| false);
+                mock_game_entity.expect_set_respawn_time().times(0);
+                mock_game_entity
+            });
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| false);
+            mock_game_entity
+                .expect_is_respawning_weapon()
+                .returning(|| false);
+            mock_game_entity.expect_set_respawn_time().times(0);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_force_weapon_respawn_time(py, 123));
+        assert!(result.is_ok_and(|value| value));
+    }
+
+    #[test]
+    #[serial]
+    fn force_weapon_respawn_time_sets_respawn_time_on_in_use_respawning_weapons() {
+        let game_entity_from_ctx = MockGameEntity::from_context();
+        game_entity_from_ctx.expect().returning(|_| {
+            let mut mock_game_entity = MockGameEntity::new();
+            mock_game_entity.expect_in_use().returning(|| true);
+            mock_game_entity
+                .expect_is_respawning_weapon()
+                .returning(|| true);
+            mock_game_entity
+                .expect_set_respawn_time()
+                .with(predicate::eq(123))
+                .times(1);
+            mock_game_entity
+        });
+
+        let result = Python::with_gil(|py| minqlx_force_weapon_respawn_time(py, 123));
+        assert!(result.is_ok_and(|value| value));
+    }
+}
