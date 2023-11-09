8d235081fe044090dc9b2dd7e54530962924e779
mgaertne <mgaertne@gmail.com>
2023-11-09 12:50:53 +0100

    got rid of the circular import error with zmq

--- python/shinqlx/_zmq.py
+++ python/shinqlx/_zmq.py
@@ -3,7 +3,7 @@ we get stats from it. It polls the ZMQ socket approx. every 0.25 seconds."""
 
 
 import zmq
-import minqlx
+import shinqlx
 
 
 class StatsListener:
@@ -11,16 +11,16 @@ class StatsListener:
         self.done = False
         self._in_progress = False
 
-        if not bool(int(minqlx.get_cvar("zmq_stats_enable"))):
+        if not bool(int(shinqlx.get_cvar("zmq_stats_enable"))):
             self.done = True
             return
 
-        host = minqlx.get_cvar("zmq_stats_ip") or "127.0.0.1"
-        port = minqlx.get_cvar("zmq_stats_port") or minqlx.get_cvar("net_port")
+        host = shinqlx.get_cvar("zmq_stats_ip") or "127.0.0.1"
+        port = shinqlx.get_cvar("zmq_stats_port") or shinqlx.get_cvar("net_port")
         self.address = f"tcp://{host}:{port}"
-        self.password = minqlx.get_cvar("zmq_stats_password")
+        self.password = shinqlx.get_cvar("zmq_stats_password")
 
-    @minqlx.delay(0.25)
+    @shinqlx.delay(0.25)
     def keep_receiving(self):
         """Receives until 'self.done' is set to True."""
         if self.done:
@@ -40,28 +40,28 @@ class StatsListener:
                 pending_events = dict(poller.poll(timeout=250))
                 for receiver in pending_events:
                     stats = receiver.recv.json()
-                    minqlx.EVENT_DISPATCHERS["stats"].dispatch(stats)
+                    shinqlx.EVENT_DISPATCHERS["stats"].dispatch(stats)
 
                     if stats["TYPE"] == "MATCH_STARTED":
                         self._in_progress = True
-                        minqlx.EVENT_DISPATCHERS["game_start"].dispatch(stats["DATA"])
+                        shinqlx.EVENT_DISPATCHERS["game_start"].dispatch(stats["DATA"])
                     elif stats["TYPE"] == "ROUND_OVER":
-                        minqlx.EVENT_DISPATCHERS["round_end"].dispatch(stats["DATA"])
+                        shinqlx.EVENT_DISPATCHERS["round_end"].dispatch(stats["DATA"])
                     elif stats["TYPE"] == "MATCH_REPORT":
                         # MATCH_REPORT event goes off with a map change and map_restart,
                         # but we really only want it for when the game actually ends.
                         # We use a variable instead of Game().state because by the
                         # time we get the event, the game is probably gone.
                         if self._in_progress:
-                            minqlx.EVENT_DISPATCHERS["game_end"].dispatch(stats["DATA"])
+                            shinqlx.EVENT_DISPATCHERS["game_end"].dispatch(stats["DATA"])
                         self._in_progress = False
                     elif stats["TYPE"] == "PLAYER_DEATH":
                         # Dead player.
                         sid = int(stats["DATA"]["VICTIM"]["STEAM_ID"])
                         player = (
-                            minqlx.Plugin.player(sid)
+                            shinqlx.Plugin.player(sid)
                             if sid
-                            else minqlx.Plugin.player(stats["DATA"]["VICTIM"]["NAME"])
+                            else shinqlx.Plugin.player(stats["DATA"]["VICTIM"]["NAME"])
                         )
 
                         # Killer player.
@@ -70,22 +70,22 @@ class StatsListener:
                         else:
                             sid_killer = int(stats["DATA"]["KILLER"]["STEAM_ID"])
                             if sid_killer:
-                                player_killer = minqlx.Plugin.player(sid_killer)
+                                player_killer = shinqlx.Plugin.player(sid_killer)
                             else:  # It's a bot. Forced to use name as an identifier.
-                                player_killer = minqlx.Plugin.player(
+                                player_killer = shinqlx.Plugin.player(
                                     stats["DATA"]["KILLER"]["NAME"]
                                 )
 
-                        minqlx.EVENT_DISPATCHERS["death"].dispatch(
+                        shinqlx.EVENT_DISPATCHERS["death"].dispatch(
                             player, player_killer, stats["DATA"]
                         )
                         if player_killer:
-                            minqlx.EVENT_DISPATCHERS["kill"].dispatch(
+                            shinqlx.EVENT_DISPATCHERS["kill"].dispatch(
                                 player, player_killer, stats["DATA"]
                             )
                     elif stats["TYPE"] == "PLAYER_SWITCHTEAM":
                         # No idea why they named it "KILLER" here, but whatever.
-                        player = minqlx.Plugin.player(
+                        player = shinqlx.Plugin.player(
                             int(stats["DATA"]["KILLER"]["STEAM_ID"])
                         )
                         if player is None:
@@ -93,7 +93,7 @@ class StatsListener:
                         old_team = stats["DATA"]["KILLER"]["OLD_TEAM"].lower()
                         new_team = stats["DATA"]["KILLER"]["TEAM"].lower()
                         if old_team != new_team:
-                            res = minqlx.EVENT_DISPATCHERS["team_switch"].dispatch(
+                            res = shinqlx.EVENT_DISPATCHERS["team_switch"].dispatch(
                                 player, old_team, new_team
                             )
                             if res is False:
