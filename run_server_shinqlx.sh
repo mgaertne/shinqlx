344332f95ecb80a6037c73b6c060d37496b9235a
mgaertne <mgaertne@gmail.com>
2024-11-07 19:23:35 +0100

    rolled back the server startup script and included it into the published artifacts
    (cherry picked from commit dd48ed887e184c0cc546bdbd1d691eb3f1bdccef)

--- run_server_shinqlx.sh
+++ run_server_shinqlx.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+MACHINE_TYPE=$(uname -m)
+if [ "${MACHINE_TYPE}" == 'x86_64' ]; then
     qlds_executable="qzeroded.x64"
 else 
     qlds_executable="qzeroded.x86"
 fi
 
-pip_location=$(pip show shinqlx | grep Location | cut -f2 -d':')
+pip_location=$(pip show shinqlx | grep Location | cut -f2 -d':' | xargs)
 if [ -z "${pip_location}" ]; then
     echo "shinqlx not found in current pip environment. Did you install it properly?"
     return 1
 fi
 
-shinqlx_lib=$(find ${pip_location}/shinqlx -name "*.so")
+shinqlx_lib=$(find "${pip_location}"/shinqlx -name "*.so")
 if [ -z "${pip_location}" ]; then
     echo "shinqlx library not found in current pip environment. Did you install it properly?"
     return 1
 fi
 
 basepath="$(dirname "$0")"
-cd $basepath
+cd "$basepath" || exit 1
 
 export LD_PRELOAD=$LD_PRELOAD:${shinqlx_lib}
 
