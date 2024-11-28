be0e7472dd3db836d8d8ce8ac97734fb13afbe85
Markus Gärtner <markus.gaertner@it-agile.de>
2024-11-28 06:11:51 +0100

    some build-script overhaul
    (cherry picked from commit d62b0016c0b1c0c2ff2bba87a5673da5e1972b10)

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+#![allow(missing_docs)]
+
 use git2::{Repository, StatusOptions, StatusShow};
 
 const COMMIT_ID_SHORT_HASH_LENGTH: usize = 8;
@@ -6,64 +8,50 @@ fn main() {
     let python_config = pyo3_build_config::get();
 
     if let Some(lib_dir) = &python_config.lib_dir {
-        println!("cargo:rustc-link-search={}", lib_dir);
+        println!("cargo::rustc-link-search={lib_dir}");
     }
 
     if let Some(library) = &python_config.lib_name {
-        println!("cargo:rustc-link-lib={}", library);
+        println!("cargo::rustc-link-lib={library}");
     }
 
     let shinqlx_version = gather_shinqlx_version();
-    println!("cargo:rustc-env=SHINQLX_VERSION={}", shinqlx_version);
+    println!("cargo::rustc-env=SHINQLX_VERSION={shinqlx_version}");
 }
 
 fn gather_shinqlx_version() -> String {
-    match Repository::discover(env!("CARGO_MANIFEST_DIR")) {
-        Err(_) => format!(
-            "\"v{}-{}\"",
-            env!("CARGO_PKG_VERSION"),
-            env!("CARGO_PKG_NAME")
-        ),
-        Ok(repository) => {
-            println!(
-                "cargo:rerun-if-changed={}",
-                repository.workdir().unwrap().display()
-            );
-
-            let modified = {
-                let statuses = repository
-                    .statuses(Some(
-                        StatusOptions::default()
-                            .show(StatusShow::IndexAndWorkdir)
-                            .include_untracked(false)
-                            .include_ignored(false)
-                            .include_unmodified(false)
-                            .exclude_submodules(false),
-                    ))
-                    .unwrap();
-                statuses.iter().any(|status| {
-                    status.status() != git2::Status::CURRENT
-                        && status.status() != git2::Status::IGNORED
-                })
-            };
-
-            if modified {
-                format!(
-                    "\"v{}-{}-modified\"",
-                    env!("CARGO_PKG_VERSION"),
-                    env!("CARGO_PKG_NAME")
-                )
-            } else {
-                let head_commit = repository.head().unwrap().peel_to_commit().unwrap();
-                let head_commit_id_str =
-                    head_commit.id().to_string()[..COMMIT_ID_SHORT_HASH_LENGTH].to_string();
-                format!(
-                    "\"v{}-{}-dev{}\"",
-                    env!("CARGO_PKG_VERSION"),
-                    env!("CARGO_PKG_NAME"),
-                    head_commit_id_str
-                )
-            }
-        }
+    let pkg_version = env!("CARGO_PKG_VERSION");
+    let pkg_name = env!("CARGO_PKG_NAME");
+    match shinqlx_version_suffix() {
+        None => format!(r#""v{pkg_version}-{pkg_name}"#),
+        Some(suffix) => format!(r#""v{pkg_version}-{pkg_name}-{suffix}"#)
     }
+}
+
+fn shinqlx_version_suffix() -> Option<String> {
+    let repository = Repository::discover(env!("CARGO_MANIFEST_DIR")).ok()?;
+
+    if let Some(workdir) = repository.workdir() {
+        println!("cargo::rerun-if-changed={}", workdir.display());
+    }
+
+    if repository
+        .statuses(Some(
+            StatusOptions::default()
+                .show(StatusShow::IndexAndWorkdir)
+                .include_untracked(false)
+                .include_ignored(false)
+                .include_unmodified(false)
+                .exclude_submodules(false),
+        )).ok()?
+        .iter()
+        .any(|status| ![git2::Status::CURRENT, git2::Status::IGNORED].contains(&status.status()))
+    {
+        return Some("modified".into());
+    }
+
+    let head_commit = repository.head().ok()?.peel_to_commit().ok()?;
+    let head_commit_id_str =
+        head_commit.id().to_string()[..COMMIT_ID_SHORT_HASH_LENGTH].to_string();
+    Some(format!("dev{head_commit_id_str}"))
 }
